4760aa625ce976a38703939c4fb0d3659bc789c3
c64-dev <c64-dev@proton.me>
2023-07-06 21:05:36 +0300

    Updated screenshot and bug fixes

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import webbrowser
 import PySimpleGUI as sg
 import subprocess
 from PIL import Image
+from random import choice
 from time import sleep
 
 
@@ -107,6 +108,9 @@ def cmd(var):
     
 def gui():
     # Create the graphical interface
+    ver = 'v1.40'
+    data_buffer = '.rep_data'
+    url = 'https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token'    
     
     col1 = sg.Column([
                 [sg.HorizontalSeparator()],
@@ -123,9 +127,9 @@ def gui():
                 [sg.T('Access Token', s=14), sg.In(key='-TOKEN-', s=40)]]), 
                 
                 sg.Column([
-                [sg.Image(key='-IMAGE-', s=(96,64), background_color='#4F5D7A')]])]), 
+                [sg.Image(key='!IMAGE!', s=(96,64), background_color='#4F5D7A')]])]), 
                 [sg.T('NOTE: This token is not saved locally. If you don\'t have a token yet, create it here:'),
-                sg.T('https://www.github.com', font='_ 11 underline', text_color='Blue', enable_events=True, key='-URL-')]
+                sg.T('https://www.github.com', font='_ 11 underline', text_color='Blue', enable_events=True, key='!URL!')]
                 ], expand_x=True)],
 
                 [sg.Frame(' Project Information ', [
@@ -140,21 +144,20 @@ def gui():
                 ], expand_x=True)],
                  
                 [sg.HorizontalSeparator()],
-                [sg.Button('COMPILE', expand_x=True), sg.Button('EXIT', s=(20))],
+                [sg.Button('Read', s=(10)), sg.Button('Write', s=(10)), sg.Button('Clear', s=(10)), sg.Button('COMPILE', expand_x=True, button_color = '#006600'), sg.Button('Exit', s=(10), button_color = '#000')],
                 [sg.StatusBar('', key='Status', s=40)]
                 ])
                 
     col2 = sg.Column([
-                [sg.Output(size=(60, 50), background_color='black', text_color='white', font='Courier 10', sbar_trough_color = 'black', sbar_background_color = 'black', sbar_arrow_color = 'black', sbar_width = 0, sbar_arrow_width = 0, key='-OUT-', expand_y=True, expand_x=True)],
-                [sg.Push(), sg.T('Made with'), sg.Image('res/love.png'), sg.T('and'), sg.Image('res/coffee.png'), sg.Push()],
-                [sg.Push(), sg.T('v1.30'), sg.Push()]
+                [sg.Output(size=(60, 50), background_color='black', text_color='white', font='Courier 10', sbar_trough_color = 'black', sbar_background_color = 'black', sbar_arrow_color = 'black', sbar_width = 0, sbar_arrow_width = 0, key='!OUT!', expand_y=True, expand_x=True)],
+                [sg.Push(), sg.T('Made with'), sg.Image('res/love.png'), sg.T('and'), sg.Image('res/coffee.png'), sg.T(' | ' + ver), sg.Push()],
+                [sg.Push(), sg.T(quote(), s=(60,3), justification='center'), sg.Push()]
                 ])
     
     layout = [sg.vtop([col1, col2])]
     
     global window  # Ugly but needed to define the window variable outside of the function
-    window = sg.Window('GitHub RepoMaker v1.30', layout, font='_ 11', resizable=False)
-    url = 'https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token'
+    window = sg.Window('GitHub RepoMaker ' + ver, layout, font='_ 11', resizable=False)
     
     prompt = window['Status'].update
     
@@ -168,7 +171,7 @@ def gui():
         try:
             for key, value in window.key_dict.items():
                 if isinstance(value, sg.In):
-                    # Make sure we have a correctly formatted email address
+                    # Make sure we have a properly formatted email address
                     if not re.match(r'[^@]+@[^@]+\.[^@]+', values['-USERMAIL-']):
                         window['-USERMAIL-'].update(background_color='#FFCCCC')
                     # Check if fields are filled in and colour accordingly
@@ -177,7 +180,7 @@ def gui():
                     else:
                         window[key].update(background_color='#FFFFFF')
                         
-            if event == sg.WIN_CLOSED or event == 'EXIT':
+            if event == sg.WIN_CLOSED or event == 'Exit':
                 if sg.Window('Warning', [[sg.Text('Do you want to quit?')], 
                     [sg.Yes(s=10), sg.No(s=10)]], keep_on_top=True).read(close=True)[0] == 'Yes':
                     break
@@ -191,9 +194,9 @@ def gui():
                         image.thumbnail((600, 400), Image.Resampling.LANCZOS)
                         bio = io.BytesIO()
                         image.save(bio, format='PNG')
-                        window['-IMAGE-'].update(data=bio.getvalue(), subsample=5)
+                        window['!IMAGE!'].update(data=bio.getvalue(), subsample=5)
                     except:
-                        window['-IMAGE-'].update(source=None)
+                        window['!IMAGE!'].update(source=None)
                         sg.Popup('Error: Selected file is not a valid image.', title = 'Warning')
                         pass                        
             elif event == '-TITLE-' and values['-TITLE-'] != '':
@@ -202,28 +205,80 @@ def gui():
                 else:
                     gui.sample_path = '~/' + values['-TITLE-'].replace(' ', '_')
                 window['-PATH1-'].update(gui.sample_path)
-                
-                
             elif event == '-PATH2-' and '/' or '\\' in values['-PATH2-']:
-                try:
-                    if os.name == 'nt':
-                        os_path = values['-PATH2-'].rsplit('\\', 1)
-                        image.save(os_path[0] + '\\screenshot.png', format='PNG')
-                    else:
-                        os_path = (values['-PATH2-'].replace(' ', '\ ')).rsplit('/', 1)
-                        image.save(os_path[0] + '/screenshot.png', format='PNG')
-                except:
+                try: 
+                    try:
+                        if os.name == 'nt':
+                            os_path = values['-PATH2-'].rsplit('\\', 1)
+                            image.save(os_path[0] + '\\screenshot.png', format='PNG')
+                        else:
+                            os_path = (values['-PATH2-'].replace(' ', '\ ')).rsplit('/', 1)
+                            image.save(os_path[0] + '/screenshot.png', format='PNG')
+                    except:
+                        pass
+                    window['-PATH2-'].update(os_path[1])
+                except IndexError:
                     pass
-                input_exec = os_path[1]                
-                window['-PATH2-'].update(input_exec)
-                
-                
-                
-                
-            elif event  == '-URL-':
+            elif event  == '!URL!':
                 print('Opening GitHub Docs page...')
                 webbrowser.open(url)
-            
+            elif event  == 'Clear':
+                if sg.Window('Warning', [[sg.Text('Do you want to clear all values?')], 
+                    [sg.Yes(s=10), sg.No(s=10)]], keep_on_top=True).read(close=True)[0] == 'Yes':
+                    prompt('Clearing all input fields.')
+                    window['-USERNAME-'].update('')
+                    window['-USERMAIL-'].update('')
+                    window['-SCRSHOT-'].update('')
+                    window['-TOKEN-'].update('')
+                    window['-TITLE-'].update('')
+                    window['-DESCR-'].update('')
+                    window['-COMMIT-'].update('')
+                    window['-PATH1-'].update('')
+                    window['-PATH2-'].update('')
+                    window['-DETAILS-'].update('')
+                    window['-FEATURES-'].update('')
+                else:
+                    prompt('')
+                    pass
+            elif event  == 'Write':
+                # Check if fields are blank or not.
+                if not any(map(str.strip, [values[key] for key in input_key_list])):
+                    prompt('Fields are empty. Nothing to write.')
+                else:
+                    prompt('Saving input fields...')
+                    tempfile = open(data_buffer, 'w')
+                    for key, value in window.key_dict.items():
+                        if isinstance(key, str) and key.startswith('-'):
+                            tempfile.write(values[key] + '±±')
+                        elif isinstance(key, str) and key.startswith('-DET'):
+                            tempfile.write(value[key] + '±±')
+                        elif isinstance(key, str) and key.startswith('-FEAT'):
+                            tempfile.write(value[key] + '±±')
+                    tempfile.close()
+            elif event == 'Read':
+                try:
+                    tempfile = open(data_buffer, 'r')
+                    t = tempfile.read().split('±±')
+                    if t == ['']:
+                        pass
+                    else:
+                        prompt('Reading input fields...')
+                        window['-USERNAME-'].update(t[0])
+                        window['-USERMAIL-'].update(t[1])
+                        window['-SCRSHOT-'].update(t[2])
+                        window['-TOKEN-'].update(t[3])
+                        window['-TITLE-'].update(t[4])
+                        window['-DESCR-'].update(t[5])
+                        window['-COMMIT-'].update(t[6])
+                        window['-PATH1-'].update(t[7])
+                        window['-PATH2-'].update(t[8])
+                        window['-DETAILS-'].update(t[9])
+                        window['-FEATURES-'].update(t[10])
+                        tempfile.close()
+                except:
+                    tempfile = open(data_buffer, 'w')
+                    tempfile.close()
+                    pass
             elif event == 'COMPILE':
                 if values['-USERMAIL-'].find('@') <= 0:
                     window['-USERMAIL-'].update(background_color='#FFCCCC')
@@ -234,6 +289,8 @@ def gui():
                 
                 if all(map(str.strip, [values[key] for key in input_key_list])):
                     prompt('All input fields are OK. Compiling...')
+                    window.refresh()
+                    sleep(1)
                     print('Running new repository compiler.')
                     window.refresh()
                     sleep(1)
@@ -278,7 +335,10 @@ def gui():
         
     window.close()
 
-
+def quote():
+    quotes_list = ['The best thing about a boolean is even if you are wrong, you are only off by a bit. (Anonymous)', 'Without requirements or design, programming is the art of adding bugs to an empty text file. (Louis Srygley)', 'Before software can be reusable it first has to be usable. (Ralph Johnson)', 'The best method for accelerating a computer is the one that boosts it by 9.8 m/s2. (Anonymous)', 'I think Microsoft named .Net so it wouldn’t show up in a Unix directory listing. (Oktal)', 'There are two ways to write error-free programs; only the third one works. (Alan J. Perlis)', 'It’s not a bug – it’s an undocumented feature. (Anonymous)', 'One man’s crappy software is another man’s full-time job. (Jessica Gaston)', 'A good programmer is someone who always looks both ways before crossing a one-way street. (Doug Linder)', 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live. (Martin Golding)', 'Programming is like sex. One mistake and you have to support it for the rest of your life. (Michael Sinz)', 'Deleted code is debugged code. (Jeff Sickel)', 'If debugging is the process of removing software bugs, then programming must be the process of putting them in. (Edsger Dijkstra)', 'Software undergoes beta testing shortly before it’s released. Beta is Latin for “still doesn’t work. (Anonymous)', 'Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to produce bigger and better idiots. So far, the universe is winning. (Rick Cook)', 'There are only two kinds of programming languages: those people always bitch about and those nobody uses. (Bjarne Stroustrup)']
+    
+    return choice(quotes_list)
+    
 if __name__ == '__main__':
-
     gui()
